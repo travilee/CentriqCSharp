bf3802368ca31681d9bab5d1d18a46a8ceb7b340
DESKTOP-J0H0BVA\Student <travisleebarnhart@gmail.com>
2018-01-29 10:00:30 -0600

    powers completed

---
+++ Powers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Class: Powers.cs – Use a FOR loop to display the powers of 2 from 1 to 50. (2 to the 1st power through 2 to the 50th power). Use a long variable to store your results and do not use methods from the Math class.
+ */
+
+namespace Powers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = 1;
+            for (long i = 1; i <= 50; i++)
+            {
+                result *= 2;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
