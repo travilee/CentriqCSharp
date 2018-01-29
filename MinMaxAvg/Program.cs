f781b521902cb7de2b39422997b35667a2fb6107
DESKTOP-J0H0BVA\Student <travisleebarnhart@gmail.com>
2018-01-29 10:08:44 -0600

    completed

---
+++ MinMaxAvg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Class: MinMaxAvg.cs – Write a program that asks the user for 5 numbers and outputs the lowest number, the highest number, and the average number. Be sure to test with a case where all numbers input are negative. 
+ */
+
+namespace MinMaxAvg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Enter number {i+1}:");
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The smallest number you entered was: {numbers.Min()}");
+            Console.WriteLine($"The highest number you entered was: {numbers.Max()}");
+            Console.WriteLine($"The average of the numbers you entered was: {numbers.Average()}");
+        }
+    }
+}
